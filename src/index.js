1d72d99582e8c8a57b70c82bb642df6d455d4b0e
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-22 21:07:21 +0530

    refactor: reorganize code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-//Todo 
-//better way to access data
-//make a readme
-
-// exports.printMsg = function () {
-//     console.log("I made changes");
-// }
-
 const airportsData = require('../airportData');
-//console.log(airportsData);
 
 exports.findAirportDetails = (airportCode) => {
-    //console.log(airportCode);
     const airportObj = airportsData.airports.reduce((Obj, curr) => {
         if (curr.code === airportCode) {
-            //console.log(curr.code);
             Obj["name"] = curr.name
             Obj["city"] = curr.city
             Obj["country"] = curr.country
-            //return Obj;
         }
         return Obj;
     }, {})
@@ -25,5 +13,3 @@ exports.findAirportDetails = (airportCode) => {
         return "Airport not found";
     else return airportObj;
 }
-
-//console.log(findAirportDetails('MMM'));
