fb3ed2e314a4306e29e5726b9129ac4358c073d3
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-22 17:56:26 +0530

    fix: use reduce

--- src/index.js
+++ src/index.js
@@ -11,16 +11,19 @@ const airportsData = require('../airportData');
 //console.log(airportsData);
 
 let findAirportDetails = (airportCode) => {
-    for(let index=0 ; index<airportsData.airports.length ; index++){
-        if(airportsData.airports[index].code === airportCode){
-            let Obj = {};
-            Obj.name = airportsData.airports[index].name;
-            Obj.city = airportsData.airports[index].city,
-            Obj.country = airportsData.airports[index].country
-            return Obj;
+    //console.log(airportCode);
+    const airportObj = airportsData.airports.reduce((Obj, curr) => {
+        if (curr.code === airportCode) {
+            //console.log(curr.code);
+            Obj["name"] = curr.name
+            Obj["city"] = curr.city
+            Obj["country"] = curr.country
+           //return Obj;
         }
-    }
-    return "Airport not found";
+        return Obj;
+    }, {})
+    if(airportObj !== {}) return airportObj;
+    else return "Airport not found";
 }
 
-console.log(findAirportDetails('DEL'));
+console.log(findAirportDetails('BSdk'));
